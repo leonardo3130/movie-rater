37fec3a2635b9a9641956f9a64b7eadce5e4fb0d
leonardo3130 <leonardopo313@gmail.com>
2026-08-18 10:28:35 +0200

    fix: fix bug in usergroup entity definition

--- MovieRaterApi/Data/Entities/UserGroup.cs
+++ MovieRaterApi/Data/Entities/UserGroup.cs
@@ -7,6 +7,6 @@ public class UserGroup
     public Guid UserId { get; set; }
     public Guid GroupId { get; set; }
 
-    public User User { get; set; } = new User();
-    public Group Group { get; set; } = new Group();
+    public User User { get; set; } = null!;
+    public Group Group { get; set; } = null!;
 }
